cfaf25c9317f1b6b2ee4d72c54dbc2875f05d26e
Selcuk Fatih Sevinc <airsfs@gmail.com>
2017-07-19 17:36:14 +0300

    v1.0.3
    * removed some middlewares

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const log = require('debug')('r2:middleware');
 const _ = require('underscore');
+const log = require('debug')('r2:middleware');
 
-module.exports = function Middleware(app, list) {
+module.exports = function Middleware(app) {
   const obj = {};
-  const getList = list || 'responseTime|helmet|compression|bodyParser|morgan|methodOverride';
+  const list = 'responseTime|helmet|compression|bodyParser|morgan';
 
-  _.each(app.utils.split(getList), (middleware) => {
+  _.each(app.utils.split(list), (middleware) => {
     try {
       require(`./lib/${middleware}`)(app); // eslint-disable-line
-      obj[middleware] = 'loaded';
+      obj[middleware] = true;
+      log(`loaded, ${middleware}`);
     } catch (e) {
       log(e);
     }
--- test/index.js
+++ test/index.js
@@ -5,7 +5,7 @@ const r2middleware = require('../index');
 const expect = chai.expect;
 const app = r2base({ baseDir: __dirname });
 app.start()
-  .serve(r2middleware, ['responseTime', 'helmet', 'compression', 'bodyParser', 'morgan', 'cors', 'methodOverride', 'cookieParser', 'csurf'])
+  .serve(r2middleware)
   .into(app);
 
 const Middleware = app.service('Middleware');
@@ -13,56 +13,23 @@ const Middleware = app.service('Middleware');
 describe('r2middleware', () => {
   describe('express middlewares', () => {
     it('should load responseTime', () => {
-      expect(Middleware.responseTime).to.equal('loaded');
+      expect(Middleware.responseTime).to.equal(true);
     });
 
     it('should load helmet', () => {
-      expect(Middleware.helmet).to.equal('loaded');
+      expect(Middleware.helmet).to.equal(true);
     });
 
     it('should load compression', () => {
-      expect(Middleware.compression).to.equal('loaded');
+      expect(Middleware.compression).to.equal(true);
     });
 
     it('should load bodyParser', () => {
-      expect(Middleware.bodyParser).to.equal('loaded');
+      expect(Middleware.bodyParser).to.equal(true);
     });
 
     it('should load morgan', () => {
-      expect(Middleware.morgan).to.equal('loaded');
-    });
-
-    it('should load cors', () => {
-      expect(Middleware.cors).to.equal('loaded');
-    });
-
-    it('should load methodOverride', () => {
-      expect(Middleware.methodOverride).to.equal('loaded');
-    });
-
-    it('should load cookieParser', () => {
-      expect(Middleware.cookieParser).to.equal('loaded');
-    });
-
-    it('should load csurf', () => {
-      expect(Middleware.csurf).to.equal('loaded');
-    });
-
-    it('should load all by default except cors, cookieParser, and csurf', () => {
-      const loadAll = r2base({ baseDir: __dirname });
-      loadAll.start()
-        .serve(r2middleware)
-        .into(loadAll);
-
-      expect(loadAll.service('Middleware').responseTime).to.equal('loaded');
-      expect(loadAll.service('Middleware').helmet).to.equal('loaded');
-      expect(loadAll.service('Middleware').compression).to.equal('loaded');
-      expect(loadAll.service('Middleware').bodyParser).to.equal('loaded');
-      expect(loadAll.service('Middleware').morgan).to.equal('loaded');
-      expect(loadAll.service('Middleware').cors).to.equal(undefined);
-      expect(loadAll.service('Middleware').methodOverride).to.equal('loaded');
-      expect(loadAll.service('Middleware').cookieParser).to.equal(undefined);
-      expect(loadAll.service('Middleware').csurf).to.equal(undefined);
+      expect(Middleware.morgan).to.equal(true);
     });
   });
 });
